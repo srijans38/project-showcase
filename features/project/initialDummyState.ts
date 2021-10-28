218d1a614cc51fe438c2e8f5f398096e936960eb
Srijan Sharma <srijans38@gmail.com>
2021-10-28 21:13:43 +0530

    refactor: dummy data

--- features/project/initialDummyState.ts
+++ features/project/initialDummyState.ts
@@ -1,83 +1,73 @@
 import { v4 as uuidv4 } from 'uuid';
 import { IProject } from './projectSlice';
 
+const dummyDesc =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.';
+
 export const dummyData: IProject[] = [
   {
     id: uuidv4(),
     title: 'Project 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 3',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 4',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 5',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 6',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 7',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 8',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 9',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 10',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 11',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 12',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
   {
     id: uuidv4(),
     title: 'Project 13',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec elit lectus. Fusce porttitor accumsan cursus. Maecenas auctor, justo sed integer.',
+    description: dummyDesc,
   },
 ];
